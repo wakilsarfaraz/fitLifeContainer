c748299eed65f78fd957c75a1ba4f087ffaa00f9
Maryam308 <mariam601me@gmail.com>
2024-11-08 02:51:41 +0300

    third commit

--- FitLife/settings.py
+++ FitLife/settings.py
@@ -14,7 +14,8 @@ from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+LOGIN_REDIRECT_URL ='/'
+LOGOUT_REDIRECT_URL ='/'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
--- FitLife/urls.py
+++ FitLife/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +26,4 @@ urlpatterns = [
     path('workouts/', include(('workouts.urls', 'workouts'), namespace='workouts')),
     path('userauth/',include('django.contrib.auth.urls')),
     path('userauth/',include(('userauth.urls','userauth'),namespace='userauth')),
-]
+]+ static (settings.MEDIA_URL, document_root =settings.MEDIA_ROOT)
