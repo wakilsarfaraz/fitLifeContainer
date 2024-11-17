1d2aef10582d12eb19014ce693eed988a7222ac5
Maryam308 <mariam601me@gmail.com>
2024-11-17 15:49:24 +0300

    testing

--- community/forms.py
+++ community/forms.py
@@ -4,4 +4,9 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['subject', 'descrip
+        fields = ['subject', 'description','image']
+
+        widgets = {
+            'subject': forms.TextInput(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Enter the description here'}),
+        }
--- community/urls.py
+++ community/urls.py
@@ -6,4 +6,7 @@ from . import views
 app_name = 'community'
 
 urlpatterns = [
-    path('
+    path('', views.PostView.as_view(), name='community'),  # List all posts
+    path('add/', views.AddPostView.as_view(), name='add_post'),  # Add new post
+    path('like/<int:post_id>/', views.like_post, name='like_post'),  # Like/unlike post
+]
