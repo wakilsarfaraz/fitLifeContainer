fd3b8eb35f553bc670ac2edda1bbb06232a3b8ae
Maryam308 <mariam601me@gmail.com>
2024-11-20 15:58:55 +0300

    working on community

--- community/views.py
+++ community/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import get_object_or_404, redirect, render
-from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
+from django.views.generic import ListView, CreateView, DetailView, View
 from django.urls import reverse_lazy
 from django.http import HttpResponseRedirect
 from django.contrib.auth.decorators import login_required
-from django.db.models import Count
 from django.contrib import messages
+from django.db.models import Count
 from .models import Post, Like, Comment
 from .forms import PostForm, CommentForm
-from django.views.generic import View
 
 # Main View for displaying posts (can be filtered by user, category, etc.)
 class PostView(ListView):
@@ -16,7 +15,8 @@ class PostView(ListView):
     context_object_name = 'posts'
 
     def get_queryset(self):
-        queryset = Post.objects.all()
+        queryset = Post.objects.all().annotate(comments_count=Count('comments'))
+
         category = self.request.GET.get('category')
         if category:
             queryset = queryset.filter(category=category)
@@ -29,9 +29,8 @@ class PostView(ListView):
         elif sort_by == 'alpha':
             queryset = queryset.order_by('title')
         elif sort_by == 'likes':
-            queryset = queryset.annotate(num_likes=Count('likes')).order_by('-num_likes')
+            queryset = queryset.order_by('-likes_count')
 
-        # Apply filter if 'my_posts' is set to 'true'
         if self.request.GET.get('my_posts') == 'true':
             queryset = queryset.filter(user=self.request.user)
 
@@ -43,13 +42,13 @@ class PostView(ListView):
         context['categories'] = Post.objects.values_list('category', flat=True).distinct()
         context['current_category'] = self.request.GET.get('category', '')
         context['current_sort'] = self.request.GET.get('sort_by', '')
-        context['my_posts'] = self.request.GET.get('my_posts') == 'true'  # To highlight the current filter state
+        context['my_posts'] = self.request.GET.get('my_posts') == 'true'
         return context
 
 # View for displaying posts made by the logged-in user (My Posts)
 @login_required
 def my_posts(request):
-    posts = Post.objects.filter(user=request.user).order_by('-date')
+    posts = Post.objects.filter(user=request.user).annotate(comments_count=Count('comments')).order_by('-date')
     return render(request, 'community.html', {'posts': posts, 'my_posts': True})
 
 # Post detail view
@@ -73,30 +72,30 @@ class AddPostView(CreateView):
         messages.success(self.request, "Your post has been added.")
         return super().form_valid(form)
 
-
 # View for deleting a post
 class DeletePostView(View):
     def post(self, request, id):
         post = get_object_or_404(Post, id=id)
-        
-        # Ensure the post is owned by the current user
         if post.user != request.user:
             messages.error(request, "You are not authorized to delete this post.")
-            return redirect('community:community')  # Or redirect to post detail, depending on your flow
+            return redirect('community:community')
         
         post.delete()
         messages.success(request, "Your post has been deleted.")
         return redirect('community:community')
-
 
 # Like/unlike a post
 @login_required
 def like_post(request, post_id):
     post = get_object_or_404(Post, id=post_id)
     like, created = Like.objects.get_or_create(user=request.user, post=post)
+    
     if not created:
         like.delete()
-    post.likes_count = post.likes.count()
+        post.likes_count -= 1
+    else:
+        post.likes_count += 1
+        
     post.save()
     messages.success(request, "You liked this post!" if created else "You unliked this post.")
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
@@ -105,33 +104,39 @@ def like_post(request, post_id):
 @login_required
 def add_comment(request, post_id):
     post = get_object_or_404(Post, id=post_id)
+
     if request.method == 'POST':
-        content = request.POST.get('content').strip()
-        if not content:
-            messages.error(request, "Comment content cannot be empty.")
+        if request.user.is_authenticated:
+            content = request.POST.get('content', '').strip()
+            if not content:
+                messages.error(request, "Comment content cannot be empty.")
+                return redirect('community:post_detail', pk=post_id)
+
+            parent_id = request.POST.get('parent_id')
+            if parent_id:
+                parent = get_object_or_404(Comment, id=parent_id)
+                Comment.objects.create(user=request.user, post=post, parent=parent, content=content)
+            else:
+                Comment.objects.create(user=request.user, post=post, content=content)
             return redirect('community:post_detail', pk=post_id)
-        
-        parent_id = request.POST.get('parent_id')
-        if parent_id:
-            parent = get_object_or_404(Comment, id=parent_id)
-            Comment.objects.create(user=request.user, post=post, parent=parent, content=content)
         else:
-            Comment.objects.create(user=request.user, post=post, content=content)
-    return redirect('community:post_detail', pk=post_id)
+            messages.warning(request, "You need to log in to add a comment.")
+            return redirect('community:post_detail', pk=post_id)
 
 # Edit a comment
 @login_required
 def edit_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
+    
     if request.user != comment.user:
         return redirect('community:post_detail', pk=comment.post.id)
-    
+
     if request.method == 'POST':
-        content = request.POST.get('content').strip()
+        content = request.POST.get('content', '').strip()
         if not content:
             messages.error(request, "Comment content cannot be empty.")
             return redirect('community:post_detail', pk=comment.post.id)
-        
+
         comment.content = content
         comment.save()
         messages.success(request, "Your comment has been updated.")
@@ -145,7 +150,7 @@ def delete_comment(request, comment_id):
     comment = get_object_or_404(Comment, id=comment_id)
     if request.user != comment.user:
         return redirect('community:post_detail', pk=comment.post.id)
-    
+
     comment.delete()
     messages.success(request, "Your comment has been deleted.")
-    return redirect('community:post_detail', pk=comment.post.id)
+   
