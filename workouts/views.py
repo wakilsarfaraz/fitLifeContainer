b38a681b8fb07efa920997c9aaf0b7b6b7422829
Maryam308 <mariam601me@gmail.com>
2024-11-20 21:57:57 +0300

    workouts added

--- workouts/views.py
+++ workouts/views.py
@@ -5,9 +5,9 @@ def workouts_view(request):
     workouts = Workout.objects.all()
     categories = Workout.objects.values_list('category', flat=True).distinct()
 
-    # Add the YouTube video ID to each workout object for easy access in the template
     for workout in workouts:
         workout.video_id = workout.get_video_id()
+        print(workout.video_id)
 
     return render(request, 'workouts.html', {
         'workouts': workouts,
