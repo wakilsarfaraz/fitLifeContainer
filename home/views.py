36eae10349f7d86d6f43a8fd2dc1891b393ddb52
Maryam308 <mariam601me@gmail.com>
2024-11-21 22:33:08 +0300

    home

--- home/views.py
+++ home/views.py
@@ -3,14 +3,14 @@ from django.shortcuts import render
 def home(request):
     context = {
         'sections': [
-            {'title': 'Classes', 'description': 'Explore our classes and take your fitness to the next level.', 'image': 'fitnessClass.webp', 'url': 'classes:classes'},
-            {'title': 'Workouts', 'description': 'Discover a variety of workouts for every fitness level.', 'image': 'workout.jpg', 'url': 'workouts:workouts'},
-            {'title': 'Community', 'description': 'Join our supportive community and achieve your goals together.', 'image': 'workouts.png', 'url': 'community:community'},
+            {'title': 'Classes', 'description': 'Explore our classes and take your fitness to the next level.', 'image': 'classes.png', 'url': 'classes:classes'},
+            {'title': 'Workouts', 'description': 'Discover a variety of workouts for every fitness level.', 'image': 'workouts.png', 'url': 'workouts:workouts'},
+            {'title': 'Community', 'description': 'Join our supportive community and achieve your goals together.', 'image': 'community.png', 'url': 'community:community'},
         ],
         'testimonials': [
-            {'name': 'John D.', 'text': 'FitLife has transformed my lifestyle. The classes are amazing and the community is so supportive!'},
-            {'name': 'Sarah M.', 'text': "I've never felt stronger or more confident. The trainers here are top-notch!"},
-            {'name': 'Mike R.', 'text': 'The variety of workouts keeps me motivated. I look forward to every session at FitLife.'},
+            {'name': 'John D.', 'text': 'FitLife has transformed my lifestyle. The classes are amazing and the community is so supportive!','image': 'user1.jpg'},
+            {'name': 'Sarah M.', 'text': "I've never felt stronger or more confident. The trainers here are top-notch!", 'image': 'user2.jpg'},
+            {'name': 'Mike R.', 'text': 'The variety of workouts keeps me motivated. I look forward to every session at FitLife.', 'image': 'user3.webp'},
         ],
         'quick_links': [
             {'title': 'Class Schedule', 'url': 'classes:classes'},
